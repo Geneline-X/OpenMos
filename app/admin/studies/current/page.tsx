a6704368d503f14072250d47c7c4c2179a10faa2
Abdullah Mustapha <a41622100@gmail.com>
2026-03-01 11:28:48 +0000

    Update current study page

--- app/admin/studies/current/page.tsx
+++ app/admin/studies/current/page.tsx
@@ -1,11 +1,12 @@
 import CurrentStudyClient from "./current-study-client";
 
-import { getActiveStudy } from "@/app/actions/studies";
+import { getActiveStudy, getStudyStats } from "@/app/actions/studies";
 
 export const dynamic = "force-dynamic";
 
 export default async function CurrentStudyPage() {
   const activeStudy = await getActiveStudy();
+  const stats = activeStudy ? await getStudyStats(activeStudy.id) : null;
 
-  return <CurrentStudyClient study={activeStudy} />;
+  return <CurrentStudyClient stats={stats} study={activeStudy} />;
 }
